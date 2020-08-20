0c98a9c6f0779cf505a715d637286d88bf961a31
Tim <tsclay9@gmail.com>
2020-08-19 22:38:44 -0400

    added support for json highlighting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,76 @@
 import React, {useEffect, useState} from 'react';
 import prettier from "prettier/standalone";
 import parserJson from "prettier/parser-babel";
+import Highlight, { defaultProps} from 'prism-react-renderer'
+import styled from 'styled-components'
+import theme from "prism-react-renderer/themes/nightOwl";
 
 import './App.css';
 
 const App = () => {
   const [episodes, setEpisodes] = useState([])
-  const [single, setSingle] = useState([{
-    "guests": [
-        "Ali Macofsky"
-    ],
-    "title": "#1526 - Ali Macofsky",
-    "episode_id": 1526,
-    "isMMA": false,
-    "isFC": false,
-    "description": "Ali Macofsky is an actress, writer, and standup comedian. Checkout her podcast \"Resting Bitch\" available on Apple Podcasts.",
-    "date": "2020-08-15T04:00:00.000Z"
-}])
+
   const fetchEpisodes = async () => {
-    const response = await fetch('/api/all')
+    const response = await fetch('/api/example')
     const data = await response.json()
     setEpisodes(data)
   }
 
   useEffect(() => {
     fetchEpisodes()
+    console.log('fetched episodes')
   }
- , [] )
+ , [])
 
-  
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow: scroll;
+  width: 50%;
+`;
+
+const Line = styled.div`
+  display: table-row;
+`;
+
+const LineNo = styled.span`
+  display: table-cell;
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
+`;
+
+const LineContent = styled.span`
+  display: table-cell;
+`;
+
   return (
     <div className="App">
       <h1>JRE</h1>
       <p>An example of a returned JSON data</p>
       {episodes.map(e => (
-        <div className="code">
-        <pre>
-          <code>
-
-       
-            {prettier.format(JSON.stringify(e), {parser: 'json-stringify', plugins: [parserJson], printWidth: 90})}
-          
-          </code>
-        </pre>
-      </div>
+         <Highlight {...defaultProps} theme={theme} code={prettier.format(JSON.stringify(e), {parser: 'json', plugins: [parserJson], printWidth: 90}).trim()} language="json">
+         {({ className, style, tokens, getLineProps, getTokenProps }) => (
+           <Pre className={className} style={style}>
+             {tokens.map((line, i) => (
+               <Line key={i} {...getLineProps({ line, key: i })}>
+                 <LineNo>{i + 1}</LineNo>
+                 <LineContent>
+                   {line.map((token, key) => (
+                     <span key={key} {...getTokenProps({ token, key })} />
+                   ))}
+                 </LineContent>
+               </Line>
+             ))}
+           </Pre>
+         )}
+       </Highlight>
       ))}
-      
     </div>
   );
 }
 
 export default App;
+
