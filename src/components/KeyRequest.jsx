0047f070f465ff628e593cae14fb82b04041f583
Tim Clay <tsclay9@gmail.com>
2020-08-28 17:28:26 -0400

    functionality for Request form in place

--- src/components/KeyRequest.jsx
+++ src/components/KeyRequest.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Button,
+  Box,
+  FormControl,
+  FormHelperText,
+  Input,
+  FormLabel,
+  InputLabel
+} from '@material-ui/core'
+import { flexbox } from '@material-ui/system'
 
 const KeyRequest = (props) => {
   const { marginBottom } = props
+  const [data, setData] = useState({})
+
+  const handleInputs = (e) => {
+    data[e.target.id] = e.target.value
+    setData(data)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log('here is the form data! ', data)
+  }
+
   return (
     <div style={marginBottom}>
       <p>Want to interact with this API?</p>
       <p>Request an API Key below!</p>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Info</legend>
+          <Box display="flex" justifyContent="start">
+            <FormControl>
+              <InputLabel htmlFor="input-name">Name</InputLabel>
+              <Input id="name" onChange={handleInputs} />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="input-name">Email</InputLabel>
+              <Input id="email" onChange={handleInputs} />
+            </FormControl>
+          </Box>
+          <Button type="submit" variant="contained" color="primary">
+            Request
+          </Button>
+        </fieldset>
+      </form>
     </div>
   )
 }
