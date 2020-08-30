cae57408246808daf308ce51473fa4d12b664e94
Tim Clay <tsclay9@gmail.com>
2020-08-30 11:40:16 -0400

    added useful alert message upon success or failure of sending api key

--- src/components/KeyRequest.jsx
+++ src/components/KeyRequest.jsx
@@ -5,12 +5,17 @@ import {
   FormControl,
   Input,
   InputLabel,
-  Typography
+  Typography,
+  Collapse
 } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert'
 
 const KeyRequest = (props) => {
   const { marginBottom, typographySpacer } = props
+  const formSpacing = { marginBottom: '1.15rem' }
   const [data, setData] = useState({})
+  const [message, setMessage] = useState('')
+  const [open, setOpen] = useState(false)
 
   const handleInputs = (e) => {
     data[e.target.id] = e.target.value
@@ -19,7 +24,29 @@ const KeyRequest = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log('here is the form data! ', data)
+    const { name, email } = data
+    try {
+      const response = await fetch('http://localhost:5000/api/v1/requestKey', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email
+        })
+      })
+      const confirm = await response.json()
+      await setMessage(confirm.message)
+    } catch (error) {
+      await setMessage(
+        'Unknown error. Please contact keymaster.jre.api@gmail.com.'
+      )
+      console.log(error)
+    } finally {
+      setOpen(true)
+    }
   }
 
   return (
@@ -33,22 +60,39 @@ const KeyRequest = (props) => {
         If you don&apos;t receive the email, please contact
         keymaster.jre.api@gmail.com for an API Key.
       </Typography>
-      <form onSubmit={handleSubmit}>
+      <form style={{ width: '50%', margin: '0 auto' }} onSubmit={handleSubmit}>
         <fieldset>
           <legend>Info</legend>
-          <Box display="flex" justifyContent="start">
-            <FormControl>
+          <Box display="flex" justifyContent="start" flexDirection="column">
+            <FormControl style={formSpacing}>
               <InputLabel htmlFor="input-name">Name</InputLabel>
               <Input id="name" onChange={handleInputs} />
             </FormControl>
-            <FormControl>
+            <FormControl style={formSpacing}>
               <InputLabel htmlFor="input-name">Email</InputLabel>
               <Input id="email" onChange={handleInputs} />
             </FormControl>
           </Box>
-          <Button type="submit" variant="contained" color="primary">
+          <Button
+            style={{ ...formSpacing, width: '100%' }}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
             Request
           </Button>
+          {message ? (
+            <Collapse in={open}>
+              <Alert
+                severity={message.match(/key sent/gi) ? 'success' : 'error'}
+                onClose={() => {
+                  setOpen(false)
+                }}
+              >
+                {message}
+              </Alert>
+            </Collapse>
+          ) : null}
         </fieldset>
       </form>
     </div>
