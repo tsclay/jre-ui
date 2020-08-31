b758ac53c3976c2d35182ac556f54063c714e8dc
Tim Clay <tsclay9@gmail.com>
2020-08-31 08:15:57 -0400

    removed unneeded code

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,6 @@ import RouteOptions from './components/Usage'
 import TechStack from './components/TechStack'
 import ErrorReporting from './components/ErrorReporting'
 import Footer from './components/Footer'
-// import * as serviceWorker from './serviceWorker';
-// import './styles/App.css'
 
 const centerBox = {
   margin: '5rem auto',
@@ -57,8 +55,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
